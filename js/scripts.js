d26b2c67bab0b74248474719fc1f12eb23afee47
djzevenbergen <djzevenbergen@gmail.com>
2020-03-30 13:32:30 -0700

    finish and fix for each functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,26 @@
 $(document).ready(function () {
+
   var items = ["one", "two", "three", "four", "five", "six"];
+  var lists = [];
 
+  $("#formOne").submit(function (event) {
+    $("#formOne").hide();
+    event.preventDefault();
+    items.forEach(function (item) {
+      lists.push(($("input#" + item).val()));
+    });
 
+    lists.sort();
 
+    var newLists = lists.map(function (list) {
+      return list.toUpperCase();
 
-})
+    });
+
+    newLists.forEach(function (newList) {
+      $("#elFinal").append("<li>" + newList + "</li>");
+
+    });
+
+  });
+});
